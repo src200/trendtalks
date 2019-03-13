34fe14c02cc12ab693f4925700d7385eefaaf412
Challa Sharath <csharath@lantronix.com>
2019-03-13 18:19:02 +0530

    add google custom search for made up query from trending repo results

--- app/app.js
+++ app/app.js
@@ -9,10 +9,6 @@ var githubRouter = require('./routes/github');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- app/routes/github.js
+++ app/routes/github.js
@@ -1,18 +1,42 @@
 var express = require('express');
 var gt = require('@huchenme/github-trending');
 var router = express.Router();
+var request = require('request');
 
 // fetch trending repositories
 router.get('/trending', function (req, res, next) {
     gt.fetchRepositories().then(function (repos) {
-        var reposList = [];
+        var promises = [];
         repos.forEach(function (repo) {
-            reposList.push({ url: repo.url, query: repo.name + ' ' + repo.author });
+            promises.push(createPromise(repo))
         });
-        res.render('index', { reposList: reposList });
+        
+        Promise.all(promises).then(function (results) {
+            res.send(results);
+        }).catch(function (error) {
+            res.send(error);
+        })
     }, function (error) {
         res.send(error);
     });
 });
+
+function createPromise(repo) {
+    return new Promise(function (resolve, reject) {
+        request('https://www.googleapis.com/customsearch/v1?key=AIzaSyDSS2JNP1_vB-pjThvbVGD7YUC_Ns7S0t4&cx=003147681764474438121:j7-pcqku6gg&q=' + repo.name + ' ' + repo.author, function (error, response, body) {
+            try {
+                var parsedBody = JSON.parse(body);
+                if (parsedBody.error) { 
+                    reject(body.error);
+                } else {
+                    resolve({ repo_url: repo.url, repo_query: repo.name + ' ' + repo.author, google_result: parsedBody });              
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+   
+}
 
 module.exports = router;
