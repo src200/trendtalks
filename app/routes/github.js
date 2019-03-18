b60373f931de869035ac7f251ac57069a8437681
Challa Sharath <sharathreddychalla@Sharathreddys-MacBook-Pro.local>
2019-03-18 22:58:45 +0530

    integrate reddit api

--- app/routes/github.js
+++ app/routes/github.js
@@ -2,13 +2,15 @@ var express = require('express');
 var gt = require('@huchenme/github-trending');
 var router = express.Router();
 var request = require('request');
+var randomUseragent = require('random-useragent');
 
 // fetch trending repositories
 router.get('/trending', function (req, res, next) {
     gt.fetchRepositories().then(function (repos) {
         var promises = [];
         repos.forEach(function (repo) {
-            promises.push(searchHackerNews(repo))
+            promises.push(searchHackerNews(repo));
+            promises.push(searchReddit(repo));
         });
         
         Promise.all(promises).then(function (results) {
@@ -22,7 +24,6 @@ router.get('/trending', function (req, res, next) {
 });
 
 // search hacker news
-// return urls for found items
 function searchHackerNews(repo) {
     return new Promise(function (resolve, reject) {
         request('https://hn.algolia.com/api/v1/search?query=' + repo.url, function (error, response, body) {
@@ -48,6 +49,48 @@ function searchHackerNews(repo) {
                 resolve(repo);
             } else {
                 repo.hnUrls = [];
+                resolve(repo);
+            }
+        });
+    });
+}
+
+
+// search reddit
+function searchReddit(repo) {
+    return new Promise(function (resolve, reject) {
+        var options = {
+            method: 'GET',
+            url: 'https://api.reddit.com/search?q=' + repo.url,
+            headers: {
+                'User-Agent': randomUseragent.getRandom()
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) {
+                reject(error);
+            }
+            
+            var _body = {}
+            try {
+                _body = JSON.parse(body);
+            } catch (e) {
+                
+            }
+
+            if (_body.data && _body.data.children && _body.data.children.length > 0) {
+                var redditUrls = [];
+                _body.data.children.forEach(function (hit) {
+                    if (hit.data && hit.data.permalink) {
+                        redditUrls.push('https://reddit.com/' + hit.data.permalink);
+                    }                    
+                });
+
+                repo.redditUrls = redditUrls; 
+                resolve(repo);
+            } else {
+                repo.redditUrls = [];
                 resolve(repo);
             }
         });
