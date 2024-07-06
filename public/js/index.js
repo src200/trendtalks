ddfe3f9f7f0e1e643717ab8af69eb6fd6e40ab96
Challa Sharath <csharath@outlook.com>
2024-07-06 11:07:59 +0530

    update rewrites

--- public/js/index.js
+++ public/js/index.js
@@ -1,116 +1,57 @@
 function fetchUrls(repo) {
     repo.repositoryName = repo.repositoryName.replace(/[^a-zA-Z ]/g, "");
 
-    $("#" + repo.repositoryName + "-loader").show();
-    $("#" + repo.repositoryName + "-btn").prop("disabled", true);
+    $(`#${repo.repositoryName}-loader`).show();
+    $(`#${repo.repositoryName}-btn`).prop("disabled", true);
 
-    var promises = [];
+    const services = [
+        { name: 'HackerNews', url: `/hn?q=${repo.url}` },
+        { name: 'Reddit', url: `/reddit?q=${repo.url}` },
+        { name: 'Stackoverflow', url: `/stackoverflow?q=${repo.url}` },
+        { name: 'Dev.to', url: `/devto?q=${repo.url}` }
+    ];
 
-    var hn = new Promise((resolve, reject) => {
-        $.ajax({
-            url: "/hn?q=" + repo.url,
-            success: (data) => {
-                resolve(data);
-            },
-            error: (error) => {
-                reject(error);
-            },
-        });
-    });
-    promises.push(hn);
+    const promises = services.map(service =>
+        fetch(service.url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => ({ name: service.name, data: data }))
+            .catch(error => ({ reason: `${service.name} failed: ${error.message}` }))
+    );
 
-    var reddit = new Promise((resolve, reject) => {
-        $.ajax({
-            url: "/reddit?q=" + repo.url,
-            success: (data) => {
-                resolve(data);
-            },
-            error: (error) => {
-                reject(error);
-            },
-        });
-    });
-    promises.push(reddit);
+    Promise.allSettled(promises)
+        .then((results) => {
+            // biome-ignore lint/complexity/noForEach: <explanation>
+            results.forEach((result) => {
+                if (result.status === 'fulfilled') {
+                    const serviceName = result.value.name;
+                    const data = result.value.data;
+                    const element = $(`#${serviceName}-${repo.repositoryName}`);
 
-    var stackoverflow = new Promise((resolve, reject) => {
-        $.ajax({
-            url: "/stackoverflow?q=" + repo.url,
-            success: (data) => {
-                resolve(data);
-            },
-            error: (error) => {
-                reject(error);
-            },
-        });
-    });
-    promises.push(stackoverflow);
-
-    var devto = new Promise((resolve, reject) => {
-        $.ajax({
-            url: "/devto?q=" + repo.url,
-            success: (data) => {
-                resolve(data);
-            },
-            error: (error) => {
-                reject(error);
-            },
-        });
-    });
-    promises.push(devto);
-
-    Promise.all(promises)
-        .then((res) => {
-            var hn = $("#hn-" + repo.repositoryName);
-            $.each(res[0], (i, v) => {
-                hn.append(
-                    "<li style='margin-bottom: 5px'><a class='repo-article' href=" +
-                    v.link +
-                    " target='_blank'>" +
-                    v.title +
-                    "</a><span class='text-gray text-muted' style='font-size: 10px'>Hacker News</span></li>",
-                );
+                    // biome-ignore lint/complexity/noForEach: <explanation>
+                    data.forEach((v) => {
+                        element.append(
+                            `<li style='margin-bottom: 5px'>
+                                <a class='repo-article' href="${v.link}" target='_blank'>${v.title}</a>
+                                <span class='text-gray text-muted' style='font-size: 10px'>${serviceName}</span>
+                            </li>`
+                        );
+                    });
+                } else {
+                    console.log(result.reason);
+                }
             });
 
-            var re = $("#reddit-" + repo.repositoryName);
-            $.each(res[1], (i, v) => {
-                re.append(
-                    "<li style='margin-bottom: 5px'><a class='repo-article' href=" +
-                    v.link +
-                    " target='_blank'>" +
-                    v.title +
-                    "</a><span class='text-gray text-muted' style='font-size: 10px'>Reddit</span></li>",
-                );
-            });
-
-            var stackoverflow = $("#stackoverflow-" + repo.repositoryName);
-            $.each(res[2], (i, v) => {
-                stackoverflow.append(
-                    "<li style='margin-bottom: 5px'><a class='repo-article' href=" +
-                    v.link +
-                    " target='_blank'>" +
-                    v.title +
-                    "</a><span class='text-gray text-muted' style='font-size: 10px'>Stack Overflow</span></li>",
-                );
-            });
-
-            var devto = $("#devto-" + repo.repositoryName);
-            $.each(res[3], (i, v) => {
-                devto.append(
-                    "<li style='margin-bottom: 5px'><a class='repo-article' href=" +
-                    v.link +
-                    " target='_blank'>" +
-                    v.title +
-                    "</a><span class='text-gray text-muted' style='font-size: 10px'>Dev.to</span></li>",
-                );
-            });
-
-
-            $("#" + repo.repositoryName + "-loader").hide();
-            $("#" + repo.repositoryName + "-btn").hide();
+            $(`#${repo.repositoryName}-loader`).hide();
+            $(`#${repo.repositoryName}-btn`).hide();
         })
         .catch((e) => {
-            console.log("Error occured", e);
-            $("#" + repo.repositoryName + "-loader").hide();
-            $("#" + repo.repositoryName + "-btn").hide();
+            console.log("Error occurred", e);
+            $(`#${repo.repositoryName}-loader`).hide();
+            $(`#${repo.repositoryName}-btn`).hide();
         });
 }
